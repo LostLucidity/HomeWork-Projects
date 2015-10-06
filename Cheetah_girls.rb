0cc36d408a2e31b3d1c842098a71f41ea4d22edc
Guelmis Cortina <guelmiscortina@Guelmis-Cortinas-MacBook-Pro.local>
2015-10-05 20:08:08 -0400

    First upload

---
+++ Cheetah_girls.rb
@@ -0,0 +1,31 @@
+ auditions = [
+  "Raven-Symone",
+  "Usher",
+  "Wiz Khalifa",
+  "Adrienne Bailon",
+  "Hulk Hogan",
+  "Sabrina Bryan",
+  "Diego Lugo",
+  "Kiely Williams",
+  "Justin Timberlake"
+]
+CHEETAH_GIRLS = [
+  "Raven-Symone",
+  "Adrienne Bailon",
+  "Sabrina Bryan",
+  "Kiely Williams"
+]
+panel = []
+i = 0
+while i < auditions.length
+	j = 0
+	while j < CHEETAH_GIRLS.length
+		if auditions[i] == CHEETAH_GIRLS[j]
+			panel << CHEETAH_GIRLS[j]
+		end
+		j += 1
+	end
+	i+= 1
+end
+
+puts panel
